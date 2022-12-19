4dada8f536fa28abba3160ba5af8eb7e7ca3fe26
basha515 <95948996+basha515@users.noreply.github.com>
2022-12-19 00:54:52 -0600

    Add files via upload

---
+++ constants.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const Pi float64 = 3.14159265355164
+
+	//const Pi float64 = 3.14159265358979323846
+	//const Pi := 3.14159265358979323846
+	//const Pi, E float64 = 3.14159265358979323846, 2.71828182845904523536
+	//const Pi = 3.14159265358979323846
+
+	fmt.Println(Pi)
+}
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("variables")
+
+	//var x int
+
+	//var x int =10
+
+	//x := 10
+
+	//var x,y,z int
+
+	//x,y,z := 1,2,3
+
+	var x string = "Hello Kick"
+
+	fmt.Println(x)
+
+	fmt.Printf("%T\n", x)
+	fmt.Printf("this is %d", x)
+}
